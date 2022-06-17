b44ca105efb6b2a51b561dfdb46608fd4b78ac17
Acep Awaludin <acep.awal.sosmed@gmail.com>
2022-06-17 16:37:37 +0700

    Membuat fungsi Add My Project dengan menggunakan method POST untuk mendapatkan data inputan (file input image diabaikan), kemudian menampilkannya ke dalam Console serta menyatukannya dengan fungsi render home atau index ('/') dengan method GET

--- index.js
+++ index.js
@@ -4,17 +4,55 @@ const port = 8000
 
 app.set('view engine','hbs')
 app.use('/assets', express.static('assets'))
+app.use(express.urlencoded({extended: false}))
 
-app.listen(port, function(){
-    console.log(`Berjalan di port ${port}`)
-})
-
-app.get('/', function(req,res){
-    res.render('index')
-})
+//. Rendering
 app.get('/contact', function(req,res){
     res.render('contact')
 })
 app.get('/add-project', function(req,res){
     res.render('add-project')
+})
+//! Render Home sudah sekalian dengan Menampilkan Project di bawah
+
+
+//. Add Project
+
+let dataProject = [
+    {
+        projectName: 'Gudangku',
+        projectStartDate: '',
+        projectEndDate: '',
+        projectDescription: ''
+    }
+]
+
+app.post('/add-project', function(req,res){
+    
+    let data = req.body
+    
+    data = {
+        projectName: data.projectName,
+        projectStartDate: data.projectStartDate,
+        projectEndDate: data.projectEndDate,
+        projectDescription: data.projectDescription
+    }
+    
+    dataProject.push(data)
+    res.redirect('/#myProject')
+})
+
+//. Menampilkan Project + Render Home
+
+app.get('/', function(req,res){
+
+    console.log(dataProject);
+
+    res.render('index')
+})
+
+
+
+app.listen(port, function(){
+    console.log(`Berjalan di port ${port}`)
 })
