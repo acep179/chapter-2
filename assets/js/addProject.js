020e41e74f417d716ff12cf9d0363668e12af26c
Acep Awaludin <acep.awal.sosmed@gmail.com>
2022-06-22 23:46:37 +0700

    Menambahkan fungsi untuk upload image menggunakan Multer baik pada addProject maupun editProject

--- assets/js/addProject.js
+++ assets/js/addProject.js
@@ -65,13 +65,88 @@ const addProject = () => {
 }
 
 const submitForm = () => {
-
+    
     let form = document.getElementById('formProject')
     form.setAttribute('action', '/add-project')
     form.setAttribute('method', 'POST')
-
+    
     let button = document.getElementById('projectButton')
     button.removeAttribute('onclick')
     button.setAttribute('type', 'submit')
-    // button.click
 }
+
+    const editProject = (id) => {
+        //. Name & Description
+        let name = document.getElementById('projectName').value
+        let description = document.getElementById('projectDescription').value
+        let startDate = document.getElementById('projectStartDate').value
+        let endDate = document.getElementById('projectEndDate').value
+        
+        // . Techologies
+        let technologies = []
+        
+        let html = document.getElementById('projectHTML').checked
+        if (html){
+            html = document.getElementById('projectHTML').value
+            technologies.push('oke')
+        }
+        
+        let css = document.getElementById('projectCSS').checked
+        if (css){
+            css = document.getElementById('projectCSS').value
+            technologies.push('oke')
+        }
+        
+        let js = document.getElementById('projectJS').checked
+        if (js){
+            js = document.getElementById('projectJS').value
+            technologies.push('oke')
+        }
+        
+        let tailwind = document.getElementById('projectTailwind').checked
+        if (tailwind){
+            tailwind = document.getElementById('projectTailwind').value
+            technologies.push('oke')
+        }
+        
+        let sass = document.getElementById('projectSASS').checked
+        if (sass){
+            sass = document.getElementById('projectSASS').value
+            technologies.push('oke')
+        }
+        
+        //. Image
+        
+        image = document.getElementById('projectImage').files
+        
+        //. Form Validation
+        
+        if(name == ''){
+            return alert('Silakan isi Nama Projek!')
+        } else if (startDate == ''){
+            return alert('Silakan tentukan tanggal memulai project!')
+        } else if (endDate == ''){
+            return alert('Silakan tentukan tanggal berakhirnya projek')
+        } else if (description == ''){
+            return alert('Kolom deskripsi masih kosong!')
+        } else if (technologies.length == 0){
+            return alert('Silakan pilih salah satu teknologi yang diterapkan!')
+        } else if(image.length == 0){
+            return alert('Silakan unggah gambar projek anda!')
+        } else {
+            image = URL.createObjectURL(image[0])
+        }
+        
+        // submitFormEdit()
+            
+            let form = document.getElementById('formProject')
+            form.setAttribute('action', `/edit-project/${id}`)
+            form.setAttribute('method', 'POST')
+            
+            let button = document.getElementById('projectButton')
+            button.removeAttribute('onclick')
+            button.setAttribute('type', 'submit')
+
+    }
+    
+    
--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 const express = require('express')
 const app = express()
+
+const multer = require('multer')
+const upload = multer({dest: 'assets/img/project'})
+    
 const port = 8000
 
 app.set('view engine','hbs')
 app.use('/assets', express.static('assets'))
 app.use(express.urlencoded({extended: false}))
 
+let isLogin = true
+
 //. Rendering
 app.get('/contact', function(req,res){
-    res.render('contact')
+    res.render('contact', {isLogin})
 })
 app.get('/add-project', function(req,res){
-    res.render('add-project')
+    res.render('add-project',{isLogin})
+})
+app.get('/edit-project/:index', function(req,res){
+
+    let index = req.params.index
+    let data = dataProject[index]
+
+    res.render('edit-project', {index, data})
 })
 //! Render Home sudah sekalian dengan Menampilkan Project di bawah
-
-let isLogin = true
 
 
 //. Add Project
@@ -27,7 +38,7 @@ let dataProject = [
         shortEndDate: '30 Mei 2022',
         monthDuration: 1,
         yearDuration: 0,
-        descriptionShort: `Gudangku adalah sebuah aplikasi manajemen gudang berbasis web. Karena`,
+        descriptionShort: `Gudangku adalah sebuah aplikasi manajemen gudang berbasis web. Karena berbasis web, aplik`,
         description: 'Gudangku adalah sebuah aplikasi manajemen gudang berbasis web. Karena berbasis web, aplikasi manajemen gudang dapat digunakan kapan pun dan di mana pun menggunakan perangkat apapun. Baik menggunakan komputer, laptop, tablet, mapun smartphone. Tentu saja, hal ini sangat memudahkan bagi pemilik bisnis sehingga tidak perlu khawatir ketika sedang ada keperluan di luar kantor.',
         technologies: ["<i class='bx bxl-html5'></i>",
         "<i class='bx bxl-css3'></i>","<i class='bx bxl-javascript'></i>"],
@@ -37,7 +48,7 @@ let dataProject = [
     }
 ]
 
-app.post('/add-project',function (req,res) {
+app.post('/add-project', upload.single("projectImage"), function (req,res,next) {
     
     let data = req.body
     
@@ -135,8 +146,7 @@ app.post('/add-project',function (req,res) {
     }
     
     //. Image
-
-    let image = data.projectImage
+    let image = req.file.path
 
     getTime(startDate,endDate)
     getDateName(startDate,endDate)
@@ -148,7 +158,7 @@ app.post('/add-project',function (req,res) {
         shortEndDate,
         monthDuration,
         yearDuration,
-        descriptionShort: description.slice(0,70),
+        descriptionShort: description.slice(0,90),
         description,
         technologies,
         technologiesName,
@@ -160,12 +170,137 @@ app.post('/add-project',function (req,res) {
     res.redirect('/#myProject')
 })
 
+//. Edit Project
+
+app.post('/edit-project/:index', upload.single("projectImage"), function (req,res) {
+    
+    let index = req.params.index
+
+    let data = req.body
+    
+    //. Name & Description
+    let name = data.projectName
+    let description = data.projectDescription
+    
+    //. Duration
+    let startDate = data.projectStartDate
+    let endDate = data.projectEndDate
+    
+    let monthDuration = ''
+    let yearDuration = ''
+
+    let shortStartDate = ''
+    let shortEndDate = ''
+
+    const getTime = (startDate, endDate) => {
+        startDate = new Date(startDate)
+        endDate = new Date(endDate)
+        
+        let getMiliSecond = endDate - startDate
+        
+        let getYearDuration = Math.floor(getMiliSecond/1000/60/60/24/365)
+        let getMonthDuration = Math.floor(getMiliSecond/1000/60/60/24/30)-(getYearDuration*12)
+        
+        if(getMonthDuration > 0){
+            monthDuration = `${getMonthDuration}`
+        }
+        
+        if(getYearDuration > 0){
+            yearDuration = `${getYearDuration}`
+        }
+    }
+    
+    const getDateName = (startDate,endDate) => {
+        startDate = new Date(startDate)
+        endDate = new Date(endDate)
+        
+        let monthArr = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Agt','Sep','Okt','Nov','Dec']
+        
+        let getStartMonth = startDate.getMonth()
+        startMonthName = monthArr[getStartMonth]
+
+        let getEndMonth = endDate.getMonth()
+        endMonthName = monthArr[getEndMonth]
+
+        startYearNumber = startDate.getFullYear()
+        endYearNumber = endDate.getFullYear()
+
+        startDateNumber = startDate.getDate()
+        endDateNumber = endDate.getDate()
+
+        shortStartDate = `${startDateNumber} ${startMonthName} ${startYearNumber}`
+        shortEndDate = `${endDateNumber} ${endMonthName} ${endYearNumber}`
+    }
+    
+    // . Techologies
+    let technologies = []
+    let technologiesName = []
+    
+    let html = Boolean(data.projectHTML)
+    if (html){
+        html = data.projectHTML
+        technologies.push(html)
+        technologiesName.push(`${html} HTML`)
+    }
+    
+    let css = Boolean(data.projectCSS)
+    if (css){
+        css = data.projectCSS
+        technologies.push(css)
+        technologiesName.push(`${css} CSS`)
+    }
+    
+    let js = Boolean(data.projectJS)
+    if (js){
+        js = data.projectJS
+        technologies.push(js)
+        technologiesName.push(`${js} JavaScript`)
+    }
+    
+    let tailwind = Boolean(data.projectTailwind)
+    if (tailwind){
+        tailwind = data.projectTailwind
+        technologies.push(tailwind)
+        technologiesName.push(`${tailwind} TailwindCSS`)
+    }
+    
+    let sass = Boolean(data.projectSASS)
+    if (sass){
+        sass = data.projectSASS
+        technologies.push(sass)
+        technologiesName.push(`${sass} SASS`)
+    }
+    
+    //. Image
+
+    let image = req.file.path
+
+    getTime(startDate,endDate)
+    getDateName(startDate,endDate)
+
+    //. Set Project Form
+    let setProjectForm = {
+        name,
+        shortStartDate,
+        shortEndDate,
+        monthDuration,
+        yearDuration,
+        descriptionShort: description.slice(0,90),
+        description,
+        technologies,
+        technologiesName,
+        image,
+        isLogin
+    }
+
+    dataProject.splice(index,1, setProjectForm)
+    res.redirect('/#myProject')
+})
 
 //. Menampilkan Project + Render Home
 
 app.get('/', function(req,res){
 
-    // console.log(dataProject)
     res.render('index',{isLogin,dataProject})
 
 })
@@ -179,6 +314,14 @@ app.get('/project-detail/:index', function(req,res){
     let project = dataProject[index]
 
     res.render('project-detail',project)
+})
+
+//. Menghapus Project
+
+app.get('/delete-project/:index', function(req,res) {
+    let index = req.params.index
+    dataProject.splice(index, 1)
+    res.redirect('/#myProject')
 })
 
 app.listen(port, function(){
