0ee497c1ab99fa1914339b7199da41278ba782dc
Acep Awaludin <acep.awal.sosmed@gmail.com>
2022-06-27 12:30:57 +0700

    Membenarkan database

--- src/connection/db.js
+++ src/connection/db.js
@@ -1,10 +1,23 @@
 const {Pool} = require('pg') //Pool tidak boleh diubah. Adapun Pool sendiri adalah sebuah properti atau function yang dimiliki oleh Postgre
 
-const dbPool = new Pool({
-    database: 'personal_web',
-    port: 5432,
-    user: 'postgres',
-    password: 'Sekar218'
-})
+
+const isProduction = process.env.NODE_ENV === "production"
+let dbPool
+
+if(isProduction){
+    dbPool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    })
+} else {
+    dbPool = new Pool({
+        database: 'personal_web',
+        port: 5432,
+        user: 'postgres',
+        password: 'Sekar218'
+    })
+}
 
 module.exports = dbPool
